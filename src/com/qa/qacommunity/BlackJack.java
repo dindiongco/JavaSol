9238aaeb1f5473fde4dacda38507d0b73bc5a442
dindiongco <dindiongco@qa.com>
2021-06-23 16:39:48 +0100

    add BlackJack.java

---
+++ src/com/qa/qacommunity/BlackJack.java
@@ -0,0 +1,31 @@
+package com.qa.qacommunity;
+
+public class BlackJack {
+
+	public static void main(String[] args) {
+		play(2, 22);
+		play(10, 20);
+		play(21, 20);
+		play(24, 1);
+	}
+	
+	public static void play(int card1, int card2) {
+		
+				
+		if (card1 <= 21 && card1 > card2) {
+			System.out.println(card1);
+		}
+		
+		else if (card1 < card2 && card2 > 21) {
+			System.out.println(card1);
+		}
+		else if (card2 <= 21) {
+			System.out.println(card2);
+		}
+		else {
+			System.out.println(0);
+		}
+		
+	}
+
+}
