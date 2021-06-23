244a53127cce4681484813bb5744c4f558c9798b
dindiongco <dindiongco@qa.com>
2021-06-23 16:46:55 +0100

    made revisions to BlackJack.java

--- src/com/qa/qacommunity/BlackJack.java
+++ src/com/qa/qacommunity/BlackJack.java
@@ -7,6 +7,8 @@ public class BlackJack {
 		play(10, 20);
 		play(21, 20);
 		play(24, 1);
+		play(22, 23);
+		play(23, 22);
 	}
 	
 	public static void play(int card1, int card2) {
@@ -16,7 +18,7 @@ public class BlackJack {
 			System.out.println(card1);
 		}
 		
-		else if (card1 < card2 && card2 > 21) {
+		else if (card1 < card2 && card2 > 21 && card1 <21) {
 			System.out.println(card1);
 		}
 		else if (card2 <= 21) {
