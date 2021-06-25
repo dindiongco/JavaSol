b8186b8340138c8a27126c20b14a112d2e8f1423
dindiongco <dindiongco@qa.com>
2021-06-25 10:39:01 +0100

    Changes and added StringManipulation.java

---
+++ src/com/qa/qacommunity/IterationsFlowcharts.java
@@ -0,0 +1,33 @@
+package com.qa.qacommunity;
+
+public class IterationsFlowcharts {
+
+	public static void main(String[] args) {
+		int i = 100;
+		int j = 100;
+		
+		for (i = 100; i < 201; i++) {
+			System.out.println("A");
+		}
+		
+		for (j = 100; j <= 200; j++) {
+			if (j%2 == 0) {
+				System.out.println("*" + "_");
+			}
+		}
+		
+	}
+	
+	public static void printNum(String[] args) {
+				
+		for(int i = 0; i < 10; i++) {
+			 
+			 System.out.println("Hello There!");
+			 }
+		
+		
+		
+	        
+	}
+
+}
---
+++ src/com/qa/qacommunity/StringManipulation.java
@@ -0,0 +1,49 @@
+package com.qa.qacommunity;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		
+		String sun = "today it is sunny, ";
+		String rain = "yesterday it was raining";
+		
+		String capSun = sun.toUpperCase();
+		String capRain = rain.toUpperCase();
+		
+		String weather = capSun.concat(capRain);
+		System.out.println(weather);
+		
+		String weatherSub = weather.substring(0,11);
+		String weatherSub2 = weather.substring(35,43);
+		
+		System.out.println(weatherSub + weatherSub2);
+		
+		count("String");
+		vertical("Hard Rock Cafe");
+		reverseVertical("Hard Rock Cafe");
+		message("Hello there", "Hello");
+
+	}
+	
+	private static void count(String a) {
+		
+		System.out.println(a.length());
+		
+	}
+	
+	private static void vertical(String a) {
+		
+		System.out.println(a.substring(0, 4) + "\n" + a.substring(5, 9) + "\n" + a.substring(10, 14));
+	}
+	
+	private static void reverseVertical(String a) {
+		
+		System.out.println(a.substring(10, 14) + "\n" + a.substring(5, 9) + "\n" + a.substring(0, 4));
+	}
+	
+	private static void message(String a, String b) {
+		
+		System.out.println(a.equals(b));
+	}
+
+}
